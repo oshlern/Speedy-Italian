d798d7cc64ca7abef583df741e7729af836ad0c9
Osher Lerner <osherler@gmail.com>
2018-03-22 10:27:26 -0700

    fixed the model and rendering in mario (now ms pacman)

--- mario.py
+++ mario.py
@@ -1,6 +1,7 @@
 # PYTHON 3 (sadly)
 import numpy
 import gym
+import time
 # import gym_pull
 # gym_pull.pull('github.com/ppaquette/gym-super-mario')        # Only required once, envs will be loaded with import gym_pull afterwards
 # env = gym.make('ppaquette/SuperMarioBros-1-1-v0')
@@ -10,17 +11,23 @@ def addToFile(file, what): # from https://stackoverflow.com/questions/13203868/h
 	f = csv.writer(open(file, 'a')).writerow(what) # appends to csv file
 
 if __name__ == "__main__": # Main part of game:
-	env = gym.make('CarRacing-v0')
+	env = gym.make('MsPacman-v0')
 	state_shape = env.observation_space.shape
 	action_size = env.action_space.n
 	agent = QNet(state_shape, action_size)
 	done = False
 	batch_size = 32
+	EPISODES = 100
+	render_rate = 15
 
-	for e in range(EPISODES): 
+	for e in range(EPISODES):
+		render = e%render_rate == 0
 		state = env.reset()
 		score = 0
 		while True:
+			if render:
+				env.render()
+				time.sleep(1/30)
 			action = agent.act(state)
 			next_state, reward, done, _ = env.step(action)
 			reward = reward if not done else -10
@@ -31,7 +38,7 @@ if __name__ == "__main__": # Main part of game:
 			if done:
 				print("episode: {}/{}, score: {}, e: {:.2}"
 					.format(e, EPISODES, score, agent.exploration))
-				addToFile("test.csv",([e, score])) # add data to file for later analization
+				addToFile("test.csv",([e, score])) # add data to file for later analyzation
 				break
 		if len(agent.memory) > batch_size:
 			agent.train(batch_size)
--- model.py
+++ model.py
@@ -1,20 +1,26 @@
-from keras.models import Sequential
-from keras.layers import Conv2D
+from keras.models import *#Sequential
+from keras.layers import *#Conv2D, Flatten, Dense
+from keras.losses import *#mean_squared_error
+from keras.optimizers import *#SGD
+import random
 from collections import deque
+import csv
 
 class QNet:
 	# Define Q neural network
-	def __init__(self, state_shape, action_size, hyperparams={'max_len': 2000, 'discount_rate': 0.99, 'exploration_init': 1.0, 'exploration_fin': 0.005, 'exploration_decay': 0.99, 'learning_rate': 0.3}):
+	def __init__(self, state_shape, action_size, hyperparams={'max_len': 2000, 'discount_rate': 0.0, 'exploration_init': 1.0, 'exploration_fin': 0.005, 'exploration_decay': 0.99, 'learning_rate': 0.000003, 'batch_size': 32}):
 		self.state_size = 1
 		for shp in state_shape:
 			self.state_size *= shp
 		self.input_shape = state_shape
 		self.action_size = action_size
 		self.memory = deque(maxlen=hyperparams['max_len'])
+		self.batch_size = hyperparams['batch_size']
 		self.discount_rate = hyperparams['discount_rate']
 		self.exploration_init = hyperparams['exploration_init']
 		self.exploration_decay = hyperparams['exploration_decay']
 		self.exploration_fin = hyperparams['exploration_fin']
+		self.exploration = self.exploration_init
 		self.learning_rate = hyperparams['learning_rate']
 		self.model = self._build_model()
 
@@ -27,7 +33,7 @@ class QNet:
 		model.add(Flatten())
 		model.add(Dense(512, activation='relu'))
 		model.add(Dense(self.action_size, activation='linear'))
-		model.compile(loss=categorical_crossentropy, # TODO: better loss, why categorical?
+		model.compile(loss=mean_squared_error, # TODO: better loss, why categorical?
 			optimizer=SGD(lr=self.learning_rate), # TODO: better optimizer, Adam
 			metrics=['accuracy'])
 		return model
@@ -47,27 +53,22 @@ class QNet:
 	
 	def train(self, action_num): # train our model
 		# training_data = list(self.memory)[len(self.memory)-action_num: len(self.memory)] # take the most recent iterations of the model
-		training_data = random.sample(list(self.memory), batch_size)
-		terminal_states, terminal_actions, terminal_rewards, nonterminal_states, nonterminal_actions, nonterminal_rewards, nonterminal_next_states = np.array([]), np.array([]), np.array([]), np.array([]), np.array([]), np.array([]), np.array([])
-		for state, action, reward, next_state, done in training_data:
-			if done:
-				np.append(terminal_states, state)
-				np.append(terminal_actions, action)
-				np.append(terminal_rewards, reward)
-			else:
-				np.append(nonterminal_states, state)
-				np.append(nonterminal_actions, action)
-				np.append(nonterminal_rewards, reward)
-				np.append(nonterminal_next_states, next_state)
+		training_data = random.sample(list(self.memory), self.batch_size)
+		states, actions, rewards, next_states, dones = zip(*training_data)
+		states, actions, rewards, next_states, dones = np.array(states), np.array(actions), np.array(rewards), np.array(next_states), np.array(dones)
+		terminal_states, terminal_actions, terminal_rewards = states[dones], actions[dones], rewards[dones]
+		not_dones = np.invert(dones)
+		nonterminal_states, nonterminal_actions, nonterminal_rewards, nonterminal_next_states = states[not_dones], actions[not_dones], rewards[not_dones], next_states[not_dones]
 		predicted_action_vals = self.model.predict(nonterminal_next_states)
 		nonterminal_rewards = nonterminal_rewards + self.discount_rate * np.amax(predicted_action_vals, axis=1) # set the Q function output as: current reward + discount factor * best future reward based on next state
-		states = terminal_states + nonterminal_states
-		rewards = terminal_rewards + nonterminal_rewards
-		actions = terminal_actions + nonterminal_actions
-		target_rewards = self.model.predict(states)
-		for i in range(batch_size):
-			target_rewards[i][actions[i]] = rewards[i][actions[i]]
-		# TODO: shuffle
+		states = np.append(terminal_states, nonterminal_states, axis=0)
+		rewards = np.append(terminal_rewards, nonterminal_rewards, axis=0)
+		actions = np.append(terminal_actions, nonterminal_actions, axis=0)
+		target_rewards = np.array(self.model.predict(states))
+		for i in range(self.batch_size):
+			target_rewards[i,actions[i]] = rewards[i]
+		reorder = np.random.permutation(self.batch_size)
+		states, target_rewards = states[reorder], target_rewards[reorder]
 		self.model.fit(states, target_rewards, epochs=1, verbose=0) # fit our model based on our state and target value
 		# decay exploration if possible
 		if self.exploration > self.exploration_fin:
